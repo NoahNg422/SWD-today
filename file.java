428a8d01ca786856603902cf53eaddc99b6829c7
Noah Nguyen <112711706+NoahNg422@users.noreply.github.com>
2022-09-03 16:25:43 -0500

    Create file.java

---
+++ file.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String some_text = "Welcome to CPSC 2740!";
+        String name = "Richard";
+        System.out.println(some_text);
+        System.out.println(15 + 27);
+        System.out.println(true);
+        System.out.println("Hello, " + name);
+        for (int c = 0; c < 99999; c++) {
+            System.out.println(c);
+        }
+    }
+}
